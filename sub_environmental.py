d46eafde8a15101cc9b066160029b21acefa5dbd
ArissaTruard <piecesofself82@gmail.com>
2025-03-21 19:06:24 -0400

    Update sub_environmental.py

--- sub_environmental.py
+++ sub_environmental.py
@@ -4,7 +4,7 @@ import datetime
 import json
 from .weather import monitor_weather
 from .water_quality_monitor import monitor_water_quality
-from .soil_quality_monitor import monitor_soil_quality
+from .soil_quality_monitor import monitor_soil_quality  # Updated import
 from .noise_monitor import monitor_noise_levels
 from .seismic_monitor import monitor_seismic_activity
 
@@ -29,19 +29,41 @@ def monitor_environment(location, weather_data=None, water_data=None, soil_data=
     analysis_results = {}
 
     if weather_data:
-        weather_results = monitor_weather(weather_data.get('humidity'), weather_data.get('temperature'), weather_data.get('uv'), weather_data.get('air_quality'), weather_data.get('allergens'), location)
+        weather_results = monitor_weather(
+            humidity=weather_data.get('humidity'),
+            temperature=weather_data.get('temperature'),
+            uv=weather_data.get('uv'),
+            air_quality=weather_data.get('air_quality'),
+            allergens=weather_data.get('allergens'),
+            location=location
+        )
         analysis_results['weather'] = weather_results
 
     if water_data:
-        water_results = monitor_water_quality(water_data.get('ph'), water_data.get('turbidity'),
-                                              water_data.get('dissolved_oxygen'), water_data.get('heavy_metals'),
-                                              water_data.get('pollutants'), location)
+        water_results = monitor_water_quality(
+            ph=water_data.get('ph'),
+            turbidity=water_data.get('turbidity'),
+            dissolved_oxygen=water_data.get('dissolved_oxygen'),
+            heavy_metals=water_data.get('heavy_metals'),
+            pollutants=water_data.get('pollutants'),
+            location=location
+        )
         analysis_results['water_quality'] = water_results
 
     if soil_data:
-        soil_results = monitor_soil_quality(soil_data.get('ph'), soil_data.get('moisture'),
-                                            soil_data.get('nutrients'), soil_data.get('heavy_metals'),
-                                            soil_data.get('pesticides'), location)
+        soil_results = monitor_soil_quality(  # Updated function call
+            moisture=soil_data.get('moisture'),
+            temperature=soil_data.get('temperature'),
+            ph=soil_data.get('ph'),
+            nitrogen=soil_data.get('nitrogen'),
+            phosphorus=soil_data.get('phosphorus'),
+            potassium=soil_data.get('potassium'),
+            heavy_metals=soil_data.get('heavy_metals'),
+            pesticides=soil_data.get('pesticides'),
+            location=location,
+            latitude=soil_data.get('latitude'),
+            longitude=soil_data.get('longitude')
+        )
         analysis_results['soil_quality'] = soil_results
 
     if noise_level is not None:
