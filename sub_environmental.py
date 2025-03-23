ef7c80144595179a938437b0f1a172877e1cbe15
ArissaTruard <piecesofself82@gmail.com>
2025-03-23 09:37:39 -0400

    Update sub_environmental.py

--- sub_environmental.py
+++ sub_environmental.py
@@ -8,32 +8,46 @@ for use in primary directives and computerized laws.
 import logging
 import datetime
 
-# Import monitor functions directly
+# Atmospheric Monitoring Modules
+from air_quality_monitor import monitor_air_quality
+from pollen_monitor import monitor_pollen_levels
 from weather import monitor_weather
+
+# Geological Monitoring Modules
+from radiation_alerts import monitor_radiation_levels
+from radon_monitor import monitor_radon_levels
+from seismic_monitor import monitor_seismic_activity
+
+# Land and Water Monitoring Modules
+from deforestation_monitor import monitor_deforestation
+from industrial_pollution_monitor import monitor_industrial_pollution
+from soil_quality_monitor import monitor_soil_quality
+from urban_sprawl_monitor import monitor_urban_sprawl
+from vegetation_monitor import monitor_vegetation
+from water_quality_monitor import monitor_water_quality
+
+# Biological Monitoring Modules
 from fauna_monitor import monitor_fauna
 from light_monitor import monitor_light_levels
 from noise_monitor import monitor_noise_levels
-from pollen_monitor import monitor_pollen_levels
-from radiation_alerts import monitor_radiation_levels
-from radon_monitor import monitor_radon_levels
-from air_quality_monitor import monitor_air_quality
-from soil_quality_monitor import monitor_soil_quality
-from vegetation_monitor import monitor_vegetation
-from water_quality_monitor import monitor_water_quality
-from seismic_monitor import monitor_seismic_activity
 
-# Import socioeconomic data modules
+# Socioeconomic Data Modules
 from crime_module import get_crime_data
 from property_value_module import get_property_values
-from school_ratings_module import get_school_ratings
 from RealWorldEconomicDataFetcher import get_economic_data
+from school_ratings_module import get_school_ratings
 
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
 class Environment:
     """Gathers and consolidates environmental and socioeconomic data."""
 
-    def __init__(self, location_input, weather_api_key=None, soil_api_key=None, vegetation_api_key=None, water_api_key=None, fauna_api_key=None, light_api_key=None, noise_api_key=None, pollen_api_key=None, radiation_api_key=None, radon_api_key=None):
+    def __init__(self, location_input,
+                 weather_api_key=None, soil_api_key=None, vegetation_api_key=None,
+                 water_api_key=None, fauna_api_key=None, light_api_key=None,
+                 noise_api_key=None, pollen_api_key=None, radiation_api_key=None,
+                 radon_api_key=None, deforestation_api_key=None,
+                 industrial_pollution_api_key=None, urban_sprawl_api_key=None):
         """Initializes the environment with location and API keys."""
         self.location_input = location_input
         self.weather_api_key = weather_api_key
@@ -46,6 +60,9 @@ class Environment:
         self.pollen_api_key = pollen_api_key
         self.radiation_api_key = radiation_api_key
         self.radon_api_key = radon_api_key
+        self.deforestation_api_key = deforestation_api_key
+        self.industrial_pollution_api_key = industrial_pollution_api_key
+        self.urban_sprawl_api_key = urban_sprawl_api_key
 
     def get_environmental_data(self):
         """Consolidates all environmental data."""
@@ -62,6 +79,9 @@ class Environment:
             'radiation': monitor_radiation_levels(self.location_input, self.radiation_api_key),
             'radon': monitor_radon_levels(self.location_input, self.radon_api_key),
             'seismic': monitor_seismic_activity(self.location_input),
+            'deforestation': monitor_deforestation(self.location_input, self.deforestation_api_key),
+            'industrial_pollution': monitor_industrial_pollution(self.location_input, self.industrial_pollution_api_key),
+            'urban_sprawl': monitor_urban_sprawl(self.location_input, self.urban_sprawl_api_key),
         }
         return env_data
 
